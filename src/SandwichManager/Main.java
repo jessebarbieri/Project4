1b6e6d32c2c81c7dbdaaa99f125d891ef7333bb5
Jesse Barbieri <jsb306@scarletmail.rutgers.edu>
2020-11-03 13:31:11 -0500

    ITS ALIVE

--- src/SandwichManager/Main.java
+++ src/SandwichManager/Main.java
@@ -3,48 +3,47 @@
  */
 package SandwichManager;
 
-<<<<<<< HEAD
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
 import javafx.stage.Stage;
-=======
+
 /**
  *
  */
-public class Main {
->>>>>>> d04c0472490b51eb7a857eb68cc6ab8142d3cd3b
+    public class Main extends Application {
 
-public class Main extends Application {
+        /**
+         * Sets up the stage for the GUI with title Transaction Manager
+         *
+         * @param primaryStage Stage for the GUI
+         * @throws Exception incase this doesn't work
+         */
+        @Override
+        public void start(Stage primaryStage) throws Exception {
+            Parent root = FXMLLoader.load(getClass().getResource("stage1.fxml"));
+            primaryStage.setTitle("Sandwich Manager");
+            primaryStage.setScene(new Scene(root, 700, 400));
+            primaryStage.show();
+        }
 
-    /**
-     * Sets up the stage for the GUI with title Transaction Manager
-     * @param primaryStage Stage for the GUI
-     * @throws Exception incase this doesn't work
-     */
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("stage1.fxml"));
-        primaryStage.setTitle("Sandwich Manager");
-        primaryStage.setScene(new Scene(root, 700, 400));
-        primaryStage.show();
+        // Not sure if this is how two stages are created
+        public void Stage2(Stage secondaryStage) throws Exception {
+            Parent root = FXMLLoader.load(getClass().getResource("stage2.fxml"));
+            secondaryStage.setTitle("Order Details");
+            secondaryStage.setScene(new Scene(root, 700, 400));
+            secondaryStage.show();
+        }
+
+
+        /**
+         * Main method that calls launch(args)
+         *
+         * @param args command line arguments
+         */
+        public static void main(String[] args) {
+            launch(args);
+        }
     }
 
-    // Not sure if this is how two stages are created
-    public void Stage2 (Stage secondaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("stage2.fxml"));
-        secondaryStage.setTitle("Order Details");
-        secondaryStage.setScene(new Scene(root, 700, 400));
-        secondaryStage.show();
-    }
-
-
-    /**
-     * Main method that calls launch(args)
-     * @param args command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
 
