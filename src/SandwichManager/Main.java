1e2da4b121f94891b95d8a6e0a6f93c6479ad680
Arya Kulkarni <kulkarya@gmail.com>
2020-11-03 15:02:51 -0500

    Done with two stages

--- src/SandwichManager/Main.java
+++ src/SandwichManager/Main.java
@@ -32,7 +32,7 @@ public class Main extends Application{
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load((getClass().getResource("/SandwichManager/createOrder.fxml")));
         primaryStage.setTitle("Sandwich Manager");
-        primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.setScene(new Scene(root, 900, 500));
         primaryStage.show();
     }
 
