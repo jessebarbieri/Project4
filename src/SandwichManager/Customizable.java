23301761ab8448dac622258f0c827578956bae4f
Jesse Barbieri <jsb306@scarletmail.rutgers.edu>
2020-11-03 13:39:01 -0500

    updating

--- src/SandwichManager/Customizable.java
+++ src/SandwichManager/Customizable.java
@@ -3,8 +3,4 @@ package SandwichManager;
 public interface Customizable {
     boolean add(Object obj);
     boolean remove(Object obj);
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> d04c0472490b51eb7a857eb68cc6ab8142d3cd3b
--- src/SandwichManager/OrderLine.java
+++ src/SandwichManager/OrderLine.java
@@ -1,10 +1,7 @@
 package SandwichManager;
 
-<<<<<<< HEAD
 import java.util.ArrayList;
 
-=======
->>>>>>> d04c0472490b51eb7a857eb68cc6ab8142d3cd3b
 public class OrderLine {
     private int lineNumber;
     private Sandwich sandwich;
