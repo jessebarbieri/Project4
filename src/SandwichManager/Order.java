78cddf2b11dd3caa1fba428b6655dbdff9eed0ba
Arya Kulkarni <kulkarya@gmail.com>
2020-11-05 15:51:12 -0500

    added skeleton for order

--- src/SandwichManager/Order.java
+++ src/SandwichManager/Order.java
@@ -6,6 +6,28 @@ public class Order implements Customizable {
     public static int lineNumber;
     private ArrayList<OrderLine> orderlines;
 
+    public Order(ArrayList<OrderLine> orderlines){
+        this.orderlines = new ArrayList<>(0);
+        lineNumber = 0;
+    }
+
+    /**
+     * Runs through orderline to search for the sandwich that needs to be removed
+     * @return boolean whether or not the sandwich has been found
+     */
+    public boolean find(Sandwich s){
+        return false;
+    }
+
+    /**
+     * Gets linenumber (order number)
+     * @return int the order number
+     */
+    public int getLineNumber(){
+        lineNumber = lineNumber + 1;
+        return lineNumber;
+    }
+
     @Override
     public boolean add(Object obj) {
         return false;
